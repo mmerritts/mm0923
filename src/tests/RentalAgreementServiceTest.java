2194f10b92dbd61fdb4c745913a0a0d461fe1c51
Marshall Merritts <marshallmerritts@gmail.com>
2023-09-17 17:17:40 -0600

    Added final 3 test scenarios

--- src/tests/RentalAgreementServiceTest.java
+++ src/tests/RentalAgreementServiceTest.java
@@ -72,4 +72,67 @@ class RentalAgreementServiceTest {
         assertEquals(BigDecimal.valueOf(1.12), testRentalAgreement.getDiscountAmount());
         assertEquals(BigDecimal.valueOf(3.35), testRentalAgreement.getFinalCharge());
     }
+
+    @Test // Test4
+    void testCheckoutDeWaltJackhammerOverLaborDayWeekendNoDiscount(){
+        var testTool = toolCreationService.getToolByToolCode("JAKD");
+        var testCheckoutRequest = new CheckoutRequest(testTool, 6, 0, LocalDate.of(2015, 9, 3));
+        var testRentalAgreement = rentalAgreementService.processCheckoutRequest(testCheckoutRequest);
+
+        testRentalAgreement.printView();
+        assertEquals("JAKD", testRentalAgreement.getToolCode());
+        assertEquals(ToolType.JACKHAMMER, testRentalAgreement.getToolType());
+        assertEquals(Brand.DEWALT, testRentalAgreement.getToolBrand());
+        assertEquals(6, testRentalAgreement.getRentalDays());
+        assertEquals(LocalDate.of(2015, 9, 3), testRentalAgreement.getCheckOutDate());
+        assertEquals(LocalDate.of(2015, 9, 9), testRentalAgreement.getDueDate());
+        assertEquals(BigDecimal.valueOf(2.99), testRentalAgreement.getDailyRentalCharge());
+        assertEquals(3, testRentalAgreement.getChargeDays());
+        assertEquals(BigDecimal.valueOf(8.97), testRentalAgreement.getPreDiscountCharge());
+        assertEquals(0, testRentalAgreement.getDiscountPercent());
+        assertEquals(BigDecimal.valueOf(0).setScale(2), testRentalAgreement.getDiscountAmount());
+        assertEquals(BigDecimal.valueOf(8.97), testRentalAgreement.getFinalCharge());
+    }
+
+    @Test // Test5
+    void testCheckoutRidgidJackhammerOverFourthJulyWeekendNoDiscount(){
+        var testTool = toolCreationService.getToolByToolCode("JAKR");
+        var testCheckoutRequest = new CheckoutRequest(testTool, 9, 0, LocalDate.of(2015, 7, 2));
+        var testRentalAgreement = rentalAgreementService.processCheckoutRequest(testCheckoutRequest);
+
+        testRentalAgreement.printView();
+        assertEquals("JAKR", testRentalAgreement.getToolCode());
+        assertEquals(ToolType.JACKHAMMER, testRentalAgreement.getToolType());
+        assertEquals(Brand.RIDGID, testRentalAgreement.getToolBrand());
+        assertEquals(9, testRentalAgreement.getRentalDays());
+        assertEquals(LocalDate.of(2015, 7, 2), testRentalAgreement.getCheckOutDate());
+        assertEquals(LocalDate.of(2015, 7, 11), testRentalAgreement.getDueDate());
+        assertEquals(BigDecimal.valueOf(2.99), testRentalAgreement.getDailyRentalCharge());
+        assertEquals(5, testRentalAgreement.getChargeDays());
+        assertEquals(BigDecimal.valueOf(14.95), testRentalAgreement.getPreDiscountCharge());
+        assertEquals(0, testRentalAgreement.getDiscountPercent());
+        assertEquals(BigDecimal.valueOf(0).setScale(2), testRentalAgreement.getDiscountAmount());
+        assertEquals(BigDecimal.valueOf(14.95), testRentalAgreement.getFinalCharge());
+    }
+
+    @Test // Test6
+    void testCheckoutRidgidJackhammerOverFourthJulyWeekend50Discount(){
+        var testTool = toolCreationService.getToolByToolCode("JAKR");
+        var testCheckoutRequest = new CheckoutRequest(testTool, 4, 50, LocalDate.of(2020, 7, 2));
+        var testRentalAgreement = rentalAgreementService.processCheckoutRequest(testCheckoutRequest);
+
+        testRentalAgreement.printView();
+        assertEquals("JAKR", testRentalAgreement.getToolCode());
+        assertEquals(ToolType.JACKHAMMER, testRentalAgreement.getToolType());
+        assertEquals(Brand.RIDGID, testRentalAgreement.getToolBrand());
+        assertEquals(4, testRentalAgreement.getRentalDays());
+        assertEquals(LocalDate.of(2020, 7, 2), testRentalAgreement.getCheckOutDate());
+        assertEquals(LocalDate.of(2020, 7, 6), testRentalAgreement.getDueDate());
+        assertEquals(BigDecimal.valueOf(2.99), testRentalAgreement.getDailyRentalCharge());
+        assertEquals(1, testRentalAgreement.getChargeDays());
+        assertEquals(BigDecimal.valueOf(2.99), testRentalAgreement.getPreDiscountCharge());
+        assertEquals(50, testRentalAgreement.getDiscountPercent());
+        assertEquals(BigDecimal.valueOf(1.5).setScale(2), testRentalAgreement.getDiscountAmount());
+        assertEquals(BigDecimal.valueOf(1.49), testRentalAgreement.getFinalCharge());
+    }
 }
